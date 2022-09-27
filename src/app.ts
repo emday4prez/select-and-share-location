5167c328bde5a1f56a3a7af68b809e97a013d641
Emerson <emerson.day@gmail.com>
2022-09-27 13:15:24 -0700

    added axios

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+import axios from 'axios'
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+
+
+function searchAddressHandler(event: Event){
+ event.preventDefault();
+ const enteredAddress = addressInput.value
+
+ // send to google api
+ axios.get();
+}
+
+
+form.addEventListener('submit', searchAddressHandler)
