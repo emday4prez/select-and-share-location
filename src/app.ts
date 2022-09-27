93612da13090f8a62d2d1150af87787036d7d1c6
Emerson <emerson.day@gmail.com>
2022-09-27 14:37:35 -0700

    finialized map functionality

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,33 @@ const API_KEY = process.env.GOOGLE_API_KEY;
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-
+type GoogleGeoCodingResponse = {
+ results: {geometry: {location: {lat: number, lng: number}}}[];
+ status: 'OK' | 'ZERO_RESULTS'
+}
 
 function searchAddressHandler(event: Event){
  event.preventDefault();
  const enteredAddress = addressInput.value
 
  // send to google api
- axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`
+ axios.get<GoogleGeoCodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`
  ).then(response => {
-  console.log(response)
+  if (response.data.status !== 'OK'){
+   throw new Error('Could not fetch location');
+  }
+  const coordinates = response.data.results[0].geometry.location;
+  const map = new google.maps.Map(document.getElementById('map')!, {
+  center: coordinates,
+  zoom: 16
+});
+
+new google.maps.Marker({
+    position: coordinates,
+    map: map,
+  });
  }).catch(err => {
+  alert(err.message)
   console.log(err)
  })
 }
